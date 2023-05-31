e5fc2f781060e3576e748fe5d5a021f74a97b4e2
ataji <tajiayoub35@gmail.com>
2023-05-31 15:56:59 -0400

    add docker-compose without volumes

--- srcs/requirements/wordpress/conf/create_wp.sh
+++ srcs/requirements/wordpress/conf/create_wp.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-wp core download --path=/var/www/ --allow-root
-cat /var/www/wp-config-sample.php > /var/www/wp-config.php
-chmod -R 0777 /var/www/wp-content
-sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
-
+mkdir -p /run/php
 chown -R www-data:www-data /var/www
-wp config --allow-root --path=/var/www/ --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$USER_PASSWORD --dbhost=$DB_HOST
-
-# wp core install --url=ataji.42.fr --path=/var/www/ --title=$TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PWD --admin_email=$ADMIN_MAIL --allow-root
-# wp user create --allow-root  --path=/var/www/  $WP_USER $WP_MAIL --role=author --user_pass=$WP_PASS
+wp core download --allow-root --path=/var/www/
+chmod -R 0777 /var/www/wp-content
+cat /var/www/wp-config-sample.php > /var/www/wp-config.php
+# wp core config --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --allow-root --path=/var/www/
+wp config set DB_NAME $DB_NAME --path=/var/www/ --allow-root
+wp config set DB_USER $DB_USER --path=/var/www/ --allow-root
+wp config set DB_PASSWORD $DB_PASS --path=/var/www/ --allow-root
+wp config set DB_HOST $DB_HOST --path=/var/www/ --allow-root
+wp core install --allow-root --url=ataji.42.fr --path=/var/www/ --title=$TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PWD --admin_email=$ADMIN_MAIL
+wp user create --allow-root  --path=/var/www/  $WP_USER $WP_MAIL --role=author --user_pass=$WP_PASS
+sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
 
 /usr/sbin/php-fpm7.4 -F
