fc028a1f4ad1dcf024fe9cc5e2e72fd3dce2724e
ataji <tajiayoub35@gmail.com>
2023-06-08 18:52:51 -0400

    add gitignore for ignore .env file

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -1,10 +1,10 @@
 service mariadb start
 cat << EOF > /tmp/init.sql
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
-CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
-GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';
+CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$USER_PASS';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
 FLUSH PRIVILEGES;
 EOF
 mysql -u root < /tmp/init.sql
-kill `cat /var/run/mysqld/mysqld.pid`
-mysqld
+service mariadb stop
+mysqld --console
--- srcs/requirements/wordpress/conf/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-    echo "hello world from the other side";
-?>
